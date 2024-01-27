c737ed69dc53986700404274ed777d9931c12310
subaru0317 <smihata@LAPTOP-5S0MOBQV>
2024-01-27 22:52:49 +0900

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,75 @@
+import { GetServerSideProps, NextPage } from "next";
+import { useEffect, useState } from "react";
+import styles from "./index.module.css";
+
+type Props = {
+  initialImageUrl: string;
+};
+
+const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
+  // useStateで状態を定義
+  const [imageUrl, setImageUrl] = useState(initialImageUrl);
+  const [loading, setLoading] = useState(false);
+  // マウント時に画像を読み込む宣言
+  useEffect(() => {
+    fetchImage().then((newImage) => {
+      setImageUrl(newImage.url);
+      setLoading(false);
+    })
+  }, []);
+  const handleClick = async () => {
+    setLoading(true);
+    const newImage = await fetchImage();
+    setImageUrl(newImage.url);
+    setLoading(false);
+  }
+  // ローディング中でなければ、画像を表示する
+  return (
+    <div className={styles.page}>
+      <button onClick={handleClick} className={styles.button}>他のにゃんこも見る</button>
+      <div className={styles.frame}>{loading || <img src={imageUrl} />}</div>
+    </div>
+  );
+};
+
+export default IndexPage;
+
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  const image = await fetchImage();
+  return {
+    props: {
+      initialImageUrl: image.url,
+    },
+  };
+};
+
+type Image = {
+  url: string;
+  // 他にも要素はあるが、必要なのはurlだけなので定義は省略
+}
+
+const fetchImage = async (): Promise<Image> => {
+  const res = await fetch("https://api.thecatapi.com/v1/images/search");
+  const images: unknown = await res.json();
+  // 配列として表現されているか？
+  if (!Array.isArray(images)) {
+    throw new Error("猫の画像が取得できませんでした");
+  }
+  const image: unknown = images[0];
+  // Imageの構造をなしているか？
+  if (!isImage(image)) {
+    throw new Error("猫の画像が取得できませんでした");
+  }
+  return image;
+}
+
+// 型ガード関数
+// isは関数スコープ外でもunknown型の絞り込みが維持できる
+const isImage = (value: unknown): value is Image => {
+  // オブジェクトか否か
+  if (!value || typeof value !== "object") {
+    return false;
+  }
+  // urlプロパティが存在かつ、型が文字列
+  return "url" in value && typeof value.url === "string";
+}
